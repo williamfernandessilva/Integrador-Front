14e406c8ddd0fe4a071be94ddea355de36c9d217
aaaaaaaaaaaa <aaaaaaaaaaaaaaaa>
2023-10-27 18:17:37 -0300

    27/10

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
+import { CadastroComponent } from './cadastro/cadastro.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AlunosComponent } from './alunos/alunos.component';
+import { CoordenadorComponent } from './coordenador/coordenador.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { ProfessoresComponent } from './professores/professores.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: 'professores', component : ProfessoresComponent},
+  {path: 'alunos',  component : AlunosComponent},
+  {path: 'coordenador',   component : CoordenadorComponent},
+  {path: 'inicio', component : InicioComponent},
+  {path: 'cadastro', component : CadastroComponent},
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,17 @@ import { AlunosComponent } from './alunos/alunos.component';
 import { ProfessoresComponent } from './professores/professores.component';
 import { CoordenadorComponent } from './coordenador/coordenador.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { InicioComponent } from './inicio/inicio.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AlunosComponent,
     ProfessoresComponent,
-    CoordenadorComponent
+    CoordenadorComponent,
+    InicioComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
